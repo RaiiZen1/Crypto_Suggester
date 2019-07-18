32d28de35cb0e288ea31271b5c8cb006faab67d2
Nicholas-t <nicholas-t.setijabudiharsa@polytechnique.edu>
2019-07-18 11:50:13 +0200

    visualize

--- test.py
+++ test.py
@@ -6,20 +6,16 @@ Created on Thu Jul 18 11:08:41 2019
 """
 
 import Bitcoin_address_suggester.get_possible_addr as sgstr
+import Bitcoin_address_suggester.viz_v2 as viz
 
-addresses=['32e3ns2dTfx5ogMYhm5UPnqsUYvu2vHGJp',    #OK
-           '3HsVXJadDZFRwQCfVFNHeornRkmZaVcDzP',
+
+#antoine's xpub address for testing
+
+addresses=['32e3ns2dTfx5ogMYhm5UPnqsUYvu2vHGJp',    #First
+           '3HsVXJadDZFRwQCfVFNHeornRkmZaVcDzP',    #OK
            '37ASdZj1gC3qKS1vLMLDQS2MdZTdYN1Kbi',
            '3GARffqjDk5LgfCUhx3L6nCk7dVEwEPUaN']
 
-addresses = ['3LoynermEyTjSx6aFTSVCCcu1CjHgHD6B8', #A
-              '3GMBMYvQBJ5VJkHxYmsGnhZEvgGSfHX14v', #B
-              '14RSprohFSLf6jud6ghW4ckhg5Jx5NaSzF', #A first
-              '14ct44kH5yVPdvCkZMPtCEyhZiuWRmfSys'] #B first
-
-addresses = ['1KfbMRm3RaNgST7aDCDxdxUH9coNxoxsTx',  #B
-  '15samGwmfwYobjxRi6akvKWDUhLVjJjUnF',#A
-  '19Em3MS2PggEoEzUhyWbh6hycFAzTFCYm8', #A
-  '1KTMbDyDANDT9FDzsxuDn4tMo6omJf6Vt7']#A first
     
-print(suggest('1KTMbDyDANDT9FDzsxuDn4tMo6omJf6Vt7'))
+print(sgstr.suggest('32e3ns2dTfx5ogMYhm5UPnqsUYvu2vHGJp'))
+viz.plot_from_first('32e3ns2dTfx5ogMYhm5UPnqsUYvu2vHGJp')
