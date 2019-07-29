78bca0c56ccc56ca48c179c56ce45680ed74537c
Nicholas-t <nicholas-t.setijabudiharsa@polytechnique.edu>
2019-07-29 16:34:19 +0200

    fix for demo

--- test.py
+++ test.py
@@ -11,21 +11,15 @@ import Bitcoin_address_suggester.viz_v2 as viz
 
 #antoine's xpub address for testing
 
-"""
 addresses=['32e3ns2dTfx5ogMYhm5UPnqsUYvu2vHGJp',    #OK
            '3HsVXJadDZFRwQCfVFNHeornRkmZaVcDzP',
            '37ASdZj1gC3qKS1vLMLDQS2MdZTdYN1Kbi',
            '3GARffqjDk5LgfCUhx3L6nCk7dVEwEPUaN']
-
-addresses = ['3LoynermEyTjSx6aFTSVCCcu1CjHgHD6B8', #A
-              '3GMBMYvQBJ5VJkHxYmsGnhZEvgGSfHX14v', #B
-              '14RSprohFSLf6jud6ghW4ckhg5Jx5NaSzF', #A first
-              '14ct44kH5yVPdvCkZMPtCEyhZiuWRmfSys'] #B first"""
-
-addresses = ['3J51RsAJBHs2wf1sYRusjwkST7pu1RSZZd',  #B
-  '1H5FB4fDZWM4fbLWS1uj9yNkV2kQyvHR9V',#A
-  '19Em3MS2PggEoEzUhyWbh6hycFAzTFCYm8', #A
-  '1KTMbDyDANDT9FDzsxuDn4tMo6omJf6Vt7']#A first
     
 #addresses = sgstr.suggest('1KTMbDyDANDT9FDzsxuDn4tMo6omJf6Vt7').keys()
-viz.plot_from_first('1KTMbDyDANDT9FDzsxuDn4tMo6omJf6Vt7')
+viz.plot_from_first('32e3ns2dTfx5ogMYhm5UPnqsUYvu2vHGJp', addresses)
+
+"""
+chances of errors:
+    new innovative wallet anonymity method to better anonymize the person
+"""
