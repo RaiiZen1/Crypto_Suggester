8e80ddae3225584ad42582e9aa9ca944a949b96d
nicholas-t <nicholas-t.setijabudiharsa@polytechnique.edu>
2019-07-30 09:58:09 +0000

    prep demo

--- test.py
+++ test.py
@@ -9,16 +9,16 @@ import Bitcoin_address_suggester.get_possible_addr as sgstr
 import Bitcoin_address_suggester.viz_v2 as viz
 
 
-#antoine's xpub address for testing
+#antoine's address for testing
 
 addresses=['32e3ns2dTfx5ogMYhm5UPnqsUYvu2vHGJp',    #OK
            '3HsVXJadDZFRwQCfVFNHeornRkmZaVcDzP',
            '37ASdZj1gC3qKS1vLMLDQS2MdZTdYN1Kbi',
            '3GARffqjDk5LgfCUhx3L6nCk7dVEwEPUaN']
     
-#addresses = sgstr.suggest('32e3ns2dTfx5ogMYhm5UPnqsUYvu2vHGJp').keys()
-#print(addresses)
-viz.plot_from_first('32e3ns2dTfx5ogMYhm5UPnqsUYvu2vHGJp', addresses)
+addresses = sgstr.suggest('32e3ns2dTfx5ogMYhm5UPnqsUYvu2vHGJp').keys()
+print(addresses)
+#viz.plot_from_first('32e3ns2dTfx5ogMYhm5UPnqsUYvu2vHGJp', addresses)
 
 """
 chances of errors:
